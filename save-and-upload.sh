93d57e6a2a8b3ed44b49eb76a76bb89ba0434871
Pascal Brax <pascalbrax+github@gmail.com>
2021-02-15 17:28:13 +0100

    Create save-and-upload.sh

---
+++ save-and-upload.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# saves webcam lungolago images every 4 seconds
+# (c) 2021 pascal brax & friedrich b.raile
+
+USER=user
+PASSWORD=PassWord
+URL=http://192.168.24.74:XXXX/jpg/image.jpg\?resolution=3072x1728
+LOCATION=My room
+FOLDER=/mnt/storage/axis
+TMPDIR=/tmp
+
+while :
+do
+        #
+        # SET UP TIME
+        NOW=$(date +"%Y-%m-%d-%H-%M-%S")
+        TODAY=$(date +"%Y-%m-%d")
+        HUMAN=$(date +"%d.%m.%Y %H:%M")
+        FILE=$NOW.jpg
+        #
+        # DOWNLOAD IMAGE FROM WEBCAM
+        echo DOWNLOADING IMAGE...
+        wget -q --show-progress --http-user=$USER --http-password=$PASSWORD $URL -O $TMPDIR/$FILE
+        #
+        # CREATE DAY FOLDER IF NOT EXIST
+        if [ ! -d "$FOLDER/$TODAY" ]; then
+                mkdir $FOLDER/$TODAY
+        fi
+        #
+        # ADD TIMESTAMP AND SAVE FILE
+        echo SAVING IMAGE...
+        convert $TMPDIR/$FILE -gravity NorthWest -pointsize 24 \
+        -fill black -annotate +3+3 "$LOCATION $HUMAN" \
+        -fill white -annotate +2+2 "$LOCATION $HUMAN" $FOLDER/$TODAY/$FILE
+        #
+        # RESIZE AND UPLOAD
+        echo RESIZING IMAGE...
+        convert $TMPDIR/$FILE -resize 30% -gravity South -crop 100x70% \
+        -pointsize 16 -gravity NorthWest \
+        -fill black -annotate +11+291 "Lungolago di Paradiso $HUMAN" \
+        -fill white -annotate +10+290 "Lungolago di Paradiso $HUMAN" $TMPDIR/$FILE
+        # SSH (for upload on remote host)
+        echo UPLOADING IMAGE...
+        scp $TMPDIR/$FILE user@web.host.example:~/example/img/webcam.jpg
+        # DELETE IMAGE AND WAIT
+        echo WAITING...
+        sleep 2s
+        rm $TMPDIR/$FILE
+
+done
